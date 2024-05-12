cc8732ae68238142e3c1dd8249e6fe6826c7c3f8
neliodass <127657011+neliodass@users.noreply.github.com>
2024-05-12 16:53:00 +0200

    Revert "Merge pull request #3 from neliodass/UserInterface-Implementation"
    This reverts commit 16fdb678a88138c55914081ea93b14954231f961, reversing
    changes made to 154d713ef492da8d59ea07b964f82cc6aec3ebba.

---
+++ GM_Util_Tool/GM_Util_Tool/PlayableCharacter.cpp
@@ -0,0 +1,9 @@
+#include "PlayableCharacter.h"
+
+PlayableCharacter::PlayableCharacter(CharacterRace characterRace, CharacterClass characterClass, const int age)
+{
+	this->characterRace = characterRace;
+	this->characterClass = characterClass;
+	this->age = age;
+}
+
---
+++ GM_Util_Tool/GM_Util_Tool/Weapon.cpp
@@ -0,0 +1,7 @@
+#include "Weapon.h"
+#include <string>
+Weapon::Weapon(std::string name,int damage)
+{
+	this->name = name;
+	this->damage = damage;
+}
