327667280fa652dd64bd651556253a677d0349f4
neliodass <127657011+neliodass@users.noreply.github.com>
2024-05-12 16:26:17 +0200

    Revert "Merge branch 'main' into UserInterface-Implementation"
    This reverts commit 6badba004dcffd6afe0f82e9173fc4f66f83e90f, reversing
    changes made to 4872636876341baa3004dfe02e074e52e8e9d49e.

--- GM_Util_Tool/GM_Util_Tool/PlayableCharacter.cpp
+++
@@ -1,9 +0,0 @@
-#include "PlayableCharacter.h"
-
-PlayableCharacter::PlayableCharacter(CharacterRace characterRace, CharacterClass characterClass, const int age)
-{
-	this->characterRace = characterRace;
-	this->characterClass = characterClass;
-	this->age = age;
-}
-
--- GM_Util_Tool/GM_Util_Tool/main.cpp
+++ GM_Util_Tool/GM_Util_Tool/main.cpp
@@ -1,15 +1,10 @@
 #include "app.h"
 #include <QtWidgets/QApplication>
-#include <PlayableCharacter.h>
-#include <Team.h>
-#include<iostream>
-
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     app w;
     w.show();
-    
     return a.exec();
 }
