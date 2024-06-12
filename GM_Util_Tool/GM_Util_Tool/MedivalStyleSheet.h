f341988f0960e34be13faef1b2b09115116b1ea6
neliodass <127657011+neliodass@users.noreply.github.com>
2024-06-12 14:32:18 +0200

    added dark theme

--- GM_Util_Tool/GM_Util_Tool/MedivalStyleSheet.h
+++ GM_Util_Tool/GM_Util_Tool/MedivalStyleSheet.h
@@ -3,31 +3,32 @@
 #ifndef UI_STYLIZATION_H
 #define UI_STYLIZATION_H
 
+#include "StyleSheet.h"
 #include <QString>
 
-class MedivalStyleSheet {
+class MedivalStyleSheet :public StyleSheet {
 public:
 	QString commonButtonStyle = "QPushButton {"
 		"margin: 3px 3px;"
 		"letter-spacing: 2px;"
-		"border-radius: 8px;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
-		"color: #ffc000;"
+		"border-radius: 0px;"
+		"font-family:Berylium;"
+		"color: #d6d6d6;"
 		"font-size: 14px;"
-		"font-weight: bold;"
+		"font-weight: regular;"
 		"text-align: center;"
 		"padding: 5px;"
-		"background: rgba(139,0,0,255);"
-		"border-top: 4px ridge #ffb000;"
-		"border-left: 4px groove #ffb000;"
-		"border-right: 4px ridge #ffb000;"
-		"border-bottom: 4px groove #ffb000;"
+		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 #B80000, stop:1 #450000);"
+		"border-top: 3px ridge #d6d6d6;"
+		"border-left: 3px groove #d6d6d6;"
+		"border-right: 3px ridge #d6d6d6;"
+		"border-bottom: 3px groove #d6d6d6;"
 		"}"
 		"QPushButton:disabled {"
 		"background: rgba(100,0,0,255);"
 		"}"
 		"QPushButton:hover {"
-		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,43,43,255), stop:1 rgba(139,0,0,255));"
+		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,43,43,255), stop:1 #450000);"
 		"}"
 		"QPushButton:active {"
 		"background: radial - gradient(circle, #ec6a6a, #e52b2b);"
@@ -40,43 +41,50 @@ public:
 	QString commonLabelStyle = "QLabel {"
 		"letter-spacing: 2px;"
 		"border-radius: 8px;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"font-family: Berylium;"
 		"font-size: 14px;"
 		"font-weight: bold;"
 		"text-align: center;"
-		"color: #ffc000;"
+		"color: #d6d6d6;"
 		"}";
 
 	QString characterList = "CharacterListWidget {"
 		"margin: 3px 3px;"
-		"color: #ffc000;"
+		"color: #d6d6d6;"
 		"padding: 5px;"
-		"background: rgba(139,0,0,255);"
-		"border: 4px solid #ffb000;"
+		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 #B80000, stop:1 #450000);"
+		"border-top: 3px ridge #d6d6d6;"
+		"border-left: 3px groove #d6d6d6;"
+		"border-right: 3px ridge #d6d6d6;"
+		"border-bottom: 3px groove #d6d6d6;"
 		"}"
 		+ commonLabelStyle +
 		"CharacterListWidget:hover {"
-		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,43,43,255), stop:1 rgba(139,0,0,255));"
+		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,43,43,255), stop:1 #450000);"
 		"}";
 
 	QString tab =
 		"QTabWidget::pane {"
-		"border: 6px solid #604020;"
-		"background: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(210, 166, 121, 255), stop:1 rgba(128, 87, 48, 255));"
+		"border-left: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 0.6 rgba(255,255,255,0),stop: 1 rgba(255,255,255,0));"
+		"border-top: 3px solid #d6d6d6;"
+		"border-right: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 1 rgba(255,255,255,0));"
+		"border-bottom: 3px solid rgba(255,255,255,0);"
+
+		"background: transparent;"
 		"}"
 		"QTabBar::tab {"
-		"color: #000000;"
-		"border-top: 6px ridge #604020;"
-		"border-left: 6px groove #604020;"
-		"border-right: 6px ridge #604020;"
-		"border-bottom: 6px groove #604020;"
+		"color: #d6d6d6;"
+		"background: rgba(139,0,0,255);"
+		"border-top: 3px ridge #d6d6d6;"
+		"border-left: 3px groove #d6d6d6;"
+		"border-right: 3px ridge #d6d6d6;"
 		"padding: 8px;"
-		"background: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(210, 166, 121, 255), stop:1 rgba(128, 87, 48, 255));"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"font-family: Berylium;"
 		"font-size: 14px;"
+		"font-weight:bold;"
 		"}"
 		"QTabBar::tab:selected, QTabBar::tab:hover {"
-		"background: qlineargradient(spread:pad, x1:0, y1:0, x2:0, y2:1, stop:0 rgba(230, 191, 148, 255), stop:1 rgba(180, 147, 104, 255));"
+		"background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,43,43,255), stop:1 rgba(139,0,0,255));"
 		"}"
 		"QTabBar::tab:!selected {"
 		"margin-top: 3px;"
@@ -89,42 +97,47 @@ public:
 		"}";
 
 	QString mainBackground = "QMainWindow {"
-		"color: #000000;"
-		"background: qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(210, 166, 121, 255), stop:0.5 rgba(180, 120, 80, 255), stop:1 rgba(128, 87, 48, 255));"
+		"color: #ffc000;"
+		"border-image:url(./resources/background_main.png) 0 0 0 0 cover ;"
 		"}";
 
 	QString frameCharacter = "QFrame {"
-		"border-top: 3px ridge #734d26;"
-		"border-left: 3px groove #734d26;"
-		"border-right: 3px ridge #734d26;"
-		"border-bottom: 3px groove #734d26;"
+		"border-left: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 0.6 rgba(255,255,255,0),stop: 1 rgba(255,255,255,0));"
+		"border-top: 3px solid #d6d6d6;"
+		"border-right: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 1 rgba(255,255,255,0));"
+		"border-bottom: 3px solid rgba(255,255,255,0);"
 		"}"
 		"QFrame > * { border: none; }";
 
 	QString normalLabel = "QLabel {"
-		"color: #000000;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"color: #d6d6d6;"
+		"font-family:Berylium;"
 		"font-size: 14px;"
+		"font-weight:bold;"
 		"}";
 
 	QString titleLabel = "QLabel {"
-		"color: #000000;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"color: #d6d6d6;"
+		"font-family:Berylium;"
+		"font-weight:bold;"
 		"font-size: 21px;"
-		"font-weight: bold;"
 		"}";
 
 	QString backstoryLabel = "QLabel {"
-		"color: #000000;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"color: #d6d6d6;"
+		"font-family:Berylium;"
+		"font-weight:bold;"
 		"font-size: 15px;"
-		"font-weight: bold;"
+
 		"}";
 
 	QString checkBox = "QCheckBox::indicator {"
 		"width: 16px;"
 		"height: 16px;"
-		"border: 2px solid #ffb000;"
+		"border-top: 2px ridge #d6d6d6;"
+		"border-left: 2px groove #d6d6d6;"
+		"border-right: 2px ridge #d6d6d6;"
+		"border-bottom: 2px groove #d6d6d6;"
 		"}"
 		"QCheckBox::indicator:unchecked  {"
 		"background: rgba(139,0,0,255);"
@@ -134,52 +147,57 @@ public:
 		"}"
 		"QCheckBox::indicator:checked { background: qradialgradient(cx:0.5, cy:0.5, radius:0.5, fx:0.5, fy:0.5, stop:0 rgba(229,210,210,255), stop:1 rgba(139,0,0,255));}"
 		"QCheckBox {"
-		"color: #000000;"
-		"font-family: Goudy Old Style, Goudy Type, serif;"
+		"color: #d6d6d6;"
+		"font-family: Berylium;"
+		"font-weight: bold;"
 		"font-size: 14px;"
 		"}";
 
-	QString inputField = "border: 3px solid #734d26; background-color: rgba(230, 191, 148, 255);";
-	QString normalInput = "QLineEdit { color: #000000; " + inputField + "}";
-	QString comboBoxInput = "QComboBox { color: #000000; font-family: Goudy Old Style, Goudy Type, serif; font-size: 14px; padding-left: 2px; " + inputField + "}"
-		"QComboBox QListView { color: #000000; font-family: Goudy Old Style, Goudy Type, serif; font-size: 14px; " + inputField + "}"
-		"QScrollBar:vertical { background-color: rgb(210, 166, 121); }"
-		"QScrollBar::handle:vertical { border: 2px solid #734d26; }"
-		"QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical { height: 0px; subcontrol-position: top; subcontrol-origin: margin; background-color: rgba(230, 191, 148, 255); }"
-		"QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical { background-color: rgba(180, 147, 104, 255); border-left: 2px groove #734d26; }";
+	QString inputField = "background-color: qlineargradient(x1 : 0, y1 : 1, x2 : 1, y2 : 1, stop : 0 #d6d6d6,stop : 0.8 #d6d6d6, stop: 1 rgba(0, 0, 0, 0)); ";
+	QString normalInput = "QLineEdit { color: #000000;font-family: Berylium; font-weight:bold; font-size: 14px; " + inputField + "}";
+	QString comboBoxInput = "QComboBox { color: #000000; font-family: Berylium; font-weight:bold; font-size: 14px; padding-left: 2px; " + inputField + "}"
+		"QComboBox QListView { color: #000000; font-family: Berylium; font-weight:bold; font-size: 14px; font-size: 14px; " + inputField + "}"
+		"QScrollBar:vertical { background-color: #C0C0C0;  }"
+		"QScrollBar::handle:vertical { border: 2px solid #7a7979;   }"
+		"QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical { height: 0px; subcontrol-position: top; subcontrol-origin: margin; background-color:#30302d; }"
+		"QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical { background-color: #30302d;  }";
 
-	QString avatarBackground = "QLabel { background-color: rgba(230, 191, 148, 255); }";
+	QString avatarBackground = "QLabel { background: transparent; }";
 	QString frameAvatar = "QFrame {"
-		"border-top: 3px ridge #734d26;"
-		"border-left: 3px groove #734d26;"
-		"border-right: 3px ridge #734d26;"
-		"border-bottom: 3px groove #734d26;}";
+		"border-top: 2px ridge #d6d6d6;"
+		"border-left: 2px groove #d6d6d6;"
+		"border-right: 2px ridge #d6d6d6;"
+		"border-bottom: 2px groove #d6d6d6;}";
 
-	QString backstory = "QTextEdit { color: #000000; margin: 1px 1px 1px 3px; " + inputField + " }"
+	QString backstory = "QTextEdit { color: black; "
+		"font-family: Berylium;"
+		"font-weight: regular;"
+		"font-size: 14px; margin: 1px 1px 1px 3px; " + inputField + " }"
 		"QScrollBar:vertical { background-color: #d2a679; }"
 		"QScrollBar::handle:vertical { border: 2px solid #734d26; }"
 		"QScrollBar::add-line:vertical, QScrollBar::sub-line:vertical { height: 0px; subcontrol-position: top; subcontrol-origin: margin; background-color: rgba(230, 191, 148, 255); }"
 		"QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical { background-color: rgba(180, 147, 104, 255); border-left: 2px groove #734d26; }";
 
 	QString scrollArea = "QScrollArea {"
-		"border-top: 3px ridge #734d26;"
-		"border-left: 3px groove #734d26;"
-		"border-right: 3px ridge #734d26;"
-		"border-bottom: 3px groove #734d26;"
+		"background:transparent;"
+		"border-left: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 0.6 rgba(255,255,255,0),stop: 1 rgba(255,255,255,0));"
+		"border-top: 3px solid #d6d6d6;"
+		"border-right: 3px solid qlineargradient(x1: 1,y1: 0,x2: 1,y2: 1,stop: 0 #d6d6d6,stop: 1 rgba(255,255,255,0));"
+		"border-bottom: 3px solid rgba(255,255,255,0);"
 		"}"
 		"QScrollArea > QWidget > QWidget {"
-		"background-color: rgba(230, 191, 148, 255);"
+		"background: transparent;"
 		"}"
 		"QScrollBar:vertical {"
 		"background-color: rgb(210, 166, 121);"
 		"}";
 
 	QString errorBox = "QMessageBox {"
-		"border-top: 5px ridge #734d26;"
-		"border-left: 5px groove #734d26;"
-		"border-right: 5px ridge #734d26;"
-		"border-bottom: 5px groove #734d26;"
-		"background-color: rgba(230, 191, 148, 255);"
+		"border-top: 2px ridge #d6d6d6;"
+		"border-left: 2px groove #d6d6d6;"
+		"border-right: 2px ridge #d6d6d6;"
+		"border-bottom: 2px groove #d6d6d6;"
+		"background-color: qlineargradient(x1: 1,y1: 1,x2: 1,y2: 0,stop: 0 #d6d6d6,stop: 0.3 rgba(255,255,255,0),stop: 1 rgba(255,255,255,0));"
 		"}"
 		+ commonButtonStyle
 		+ normalLabel;
--- GM_Util_Tool/GM_Util_Tool/main.cpp
+++ GM_Util_Tool/GM_Util_Tool/main.cpp
@@ -1,6 +1,7 @@
 #include "GM_Util_Tool.h"
 #include<iostream>
 #include <PlayableCharacter.h>
+#include<QFontDatabase>
 #include <QtWidgets/QApplication>
 #include <Team.h>
 
@@ -8,6 +9,7 @@ int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
 	GM_Util_Tool w;
+	QFontDatabase::addApplicationFont("./resources/Berylium.ttf");
 	w.set_style();
 	w.show();
 	return a.exec();
